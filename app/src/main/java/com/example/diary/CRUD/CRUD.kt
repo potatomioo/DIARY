ab66b3554fb9ea264fe4696073cd3fec9f5e6d27
Deeptanshu Shukla <deeptanshushuklaji@gmail.com>
2024-08-13 04:40:16 +0530

    CR function Firebse

--- app/src/main/java/com/example/diary/CRUD/CRUD.kt
+++ app/src/main/java/com/example/diary/CRUD/CRUD.kt
@@ -1,42 +1,42 @@
-//package com.example.diary.CRUD
-//
-//import com.example.diary.Notes
-//import com.google.firebase.Firebase
-//import com.google.firebase.firestore.firestore
-//import org.w3c.dom.NameList
-//import java.util.jar.Attributes.Name
-//
-//private val db = Firebase.firestore
-//
-//fun addNote(note: Notes){
-//    val thisnote = hashMapOf(
-//        "Title" to note.Title,
-//        "note" to note.note
-//    )
-//    db.collection("notes")
-//        .add(thisnote)
-//        .addOnSuccessListener { documentRef ->
-//            println("Added with id ${documentRef}")
-//        }
-//        .addOnFailureListener{e->
-//            println("Falure ${e}")
-//        }
-//}
-//
-//fun getList(
-//    NameList : MutableList<String>
-//){
-//    db.collection("notes")
-//        .get()
-//        .addOnSuccessListener { ref->
-//            NameList.clear()
-//            for(doc in ref){
-//                val Fname = doc.getString("first")?:"Unknown"
-//                val Lname = doc.getString("last")?:"Unknown"
-//                NameList.add("${Fname} ${Lname}")
-//            }
-//        }
-//        .addOnFailureListener{e->
-//            println("Falure ${e}")
-//        }
-//}
+package com.example.diary.CRUD
+
+import com.example.diary.Notes
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import org.w3c.dom.NameList
+import java.util.jar.Attributes.Name
+
+private val db = Firebase.firestore
+
+fun addNote(note: Notes){
+    val thisnote = hashMapOf(
+        "Title" to note.Title,
+        "note" to note.note
+    )
+    db.collection("notes")
+        .add(thisnote)
+        .addOnSuccessListener { documentRef ->
+            println("Added with id ${documentRef}")
+        }
+        .addOnFailureListener{e->
+            println("Falure ${e}")
+        }
+}
+
+fun getList(
+    NameList : MutableList<String>
+){
+    db.collection("notes")
+        .get()
+        .addOnSuccessListener { ref->
+            NameList.clear()
+            for(doc in ref){
+                val Fname = doc.getString("first")?:"Unknown"
+                val Lname = doc.getString("last")?:"Unknown"
+                NameList.add("${Fname} ${Lname}")
+            }
+        }
+        .addOnFailureListener{e->
+            println("Falure ${e}")
+        }
+}
